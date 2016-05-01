af0fe07daa73b2504681d56f3f5a3a689039f677
cchen2018 <chris.chen796@gmail.com>
2016-05-01 19:09:42 -0400

    made date formate readable

--- cleaning.R
+++ cleaning.R
@@ -9,32 +9,32 @@ date_ordered <- raw[order(as.Date(raw$game_date, format="%m/%d/%Y")),]
 # create new data frame with normalized dates
 date_normal = date_ordered
 # reformat data into more R-readable format
-date_normal$game_date = as.Date(date_normal$game_date, format="%m/%d/%Y")
-date_normal$normal_date = 0
+date_normal$game_date_formatted = as.Date(date_normal$game_date, format="%m/%d/%Y")
+date_normal$game_number = 0
 
 # loop through and normalize the dates
-date = date_normal$game_date[1]
+date = date_normal$game_date_formatted[1]
 counter = 1
-for (i in 1:length(date_normal$game_date)){
-  date1 = date_normal$game_date[i]
+for (i in 1:length(date_normal$game_date_formatted)){
+  date1 = date_normal$game_date_formatted[i]
   if (date1 != date) {
     date = date1
     counter = counter + 1
   }
-  date_normal$normal_date[i] = counter
+  date_normal$game_number[i] = counter
 }
 
 # make sure normalized dates are sorted
-is.unsorted(date_normal$normal_date) # Should return false
+is.unsorted(date_normal$game_number) # Should return false
 
 # calculate percentage of shots made each game and merge into date_normal data frame
-dates = subset(date_normal, select = c(normal_date,shot_made_flag))
-dates_unique = unique(subset(dates, select = c(normal_date)))
+dates = subset(date_normal, select = c(game_number,shot_made_flag))
+dates_unique = unique(subset(dates, select = c(game_number)))
 dates_unique$avg = 0
 dates_unique$shots_made = 0
 dates_unique$shots_taken = 0
-for (i in 1:length(dates_unique$normal_date)){
-  shots_attempted = subset(dates, normal_date == i)$shot_made_flag
+for (i in 1:length(dates_unique$game_number)){
+  shots_attempted = subset(dates, game_number == i)$shot_made_flag
   dates_unique$shots_made[i] = sum(shots_attempted)
   dates_unique$avg[i] = mean(shots_attempted)
   dates_unique$shots_taken[i] = length(shots_attempted)
@@ -46,14 +46,14 @@ clutch_threshold = 1
 dates_unique$clutch_perc = 0
 dates_unique$clutch_shots_made = 0
 dates_unique$clutch_shots_taken = 0
-for (i in 1:length(dates_unique$normal_date)){
+for (i in 1:length(dates_unique$game_number)){
   clutches_attempted = subset(date_normal, 
-                        normal_date == i & minutes_remaining <= clutch_threshold)$shot_made_flag
+                        game_number == i & minutes_remaining <= clutch_threshold)$shot_made_flag
   dates_unique$clutch_perc[i] = mean(clutches_attempted)
   dates_unique$clutch_shots_made[i] = sum(clutches_attempted)
   dates_unique$clutch_shots_taken[i] = length(clutches_attempted)
 }
-clutch_shooting = subset(dates_unique, select = c(normal_date, clutch_perc, clutch_shots_made, clutch_shots_taken))
+clutch_shooting = subset(dates_unique, select = c(game_number, clutch_perc, clutch_shots_made, clutch_shots_taken))
 # filter out NaNs from clutch_shooting percentages
 clutch_shooting = subset(clutch_shooting, 
                          !is.nan(clutch_perc) & !is.nan(clutch_shots_made) & !is.nan(clutch_shots_taken))
@@ -63,9 +63,9 @@ dates_unique$ot = 0
 dates_unique$ot_taken = 0
 dates_unique$ot_made = 0
 dates_unique$ot_avg = 0
-for (i in 1:length(dates_unique$normal_date)){
+for (i in 1:length(dates_unique$game_number)){
   overtime = subset(date_normal, 
-                    normal_date == i & period > 4)$shot_made_flag
+                    game_number == i & period > 4)$shot_made_flag
   if (length(overtime) == 0) {
     dates_unique$ot[i] = 0
   }
@@ -79,4 +79,4 @@ for (i in 1:length(dates_unique$normal_date)){
 
 # add avg and total shots made to date_normal dataframe
 # write.csv(date_normal, "/Users/ChrisChen/Desktop/cleaned.csv")
-date_normal = merge(date_normal, dates_unique, by="normal_date")
+date_normal = merge(date_normal, dates_unique, by="game_number")
