f24f22390337effd175f23aaade03504b13232dc
jfelizardo <jfelizardo@college.harvard.edu>
2016-05-02 15:07:41 -0400

    Checked for normality, need to figure out how to bootstrap

--- analysis.R
+++ analysis.R
@@ -1,5 +1,6 @@
 mydata = read.csv("https://raw.githubusercontent.com/xueharry/Stat-139-Final-Project/master/cleaned.csv", header = T)
 clutch_shots = read.csv("https://raw.githubusercontent.com/xueharry/Stat-139-Final-Project/master/clutch_shots.csv", header = T)
+bydate_data = read.csv("https://raw.githubusercontent.com/xueharry/Stat-139-Final-Project/master/by_date.csv", header = T)
 ## to analyze whether clutch shot percentage is higher in wins versus losses
 t.test(clutch_shots$clutch_perc~clutch_shots$win)
 ## shot percentage is higher in wins
@@ -9,3 +10,9 @@ t.test(clutch_shots$clutch_shots_taken~clutch_shots$win)
 ## does Kobe make more clutch shots in wins if he takes less shots?
 t.test(clutch_shots$clutch_shots_made~clutch_shots$win)
 ## no significant difference in clutch shots made between wins and losses
+## check for normality of the response variables
+ad.test(clutch_shots$clutch_perc)
+ad.test(clutch_shots$clutch_shots_made)
+ad.test(clutch_shots$clutch_shots_taken)
+## none of these variables are normal, bootstrap to account for it
+
