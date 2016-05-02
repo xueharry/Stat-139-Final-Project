0751aebc28dc51ca5deac9ddd7c2ca896805252d
jfelizardo <jfelizardo@college.harvard.edu>
2016-05-02 14:29:01 -0400

    Performed preliminary analysis using t.test, looking for significant differences between wins and losses

--- analysis.R
+++ analysis.R
@@ -1,2 +1,11 @@
 mydata = read.csv("https://raw.githubusercontent.com/xueharry/Stat-139-Final-Project/master/cleaned.csv", header = T)
 clutch_shots = read.csv("https://raw.githubusercontent.com/xueharry/Stat-139-Final-Project/master/clutch_shots.csv", header = T)
+## to analyze whether clutch shot percentage is higher in wins versus losses
+t.test(clutch_shots$clutch_perc~clutch_shots$win)
+## shot percentage is higher in wins
+## to analyze whether Kobe takes more clutch shots in wins versus losses
+t.test(clutch_shots$clutch_shots_taken~clutch_shots$win)
+## number of shots taken is higher in losses versus wins
+## does Kobe make more clutch shots in wins if he takes less shots?
+t.test(clutch_shots$clutch_shots_made~clutch_shots$win)
+## no significant difference in clutch shots made between wins and losses
