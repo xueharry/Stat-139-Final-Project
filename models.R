b411d950f5369636ea7e5d733f4ef13e19a746a8
bxhafa <bxhafa@college.harvard.edu>
2016-05-03 02:53:57 -0400

    Regressions
    I did a logit regression and a profit regression and checked for
    stationarity here. I need to run a regression on win data next and
    possibly validate these. Although, I think cross-validation will
    actually not work well for these regressions.

--- models.R
+++ models.R
@@ -1,2 +1,42 @@
 mydata = read.csv("https://raw.githubusercontent.com/xueharry/Stat-139-Final-Project/master/cleaned.csv", header = T)
 clutch_shots = read.csv("https://raw.githubusercontent.com/xueharry/Stat-139-Final-Project/master/clutch_shots.csv", header = T)
+
+# subset data
+myvars=c("game_number","loc_x","loc_y","minutes_remaining","period","playoffs","seconds_remaining","shot_distance","shot_made_flag","win","home","three_pointer","jump_shot","dunk","tip_shot","hook_shot","bank_shot")
+data=cleaned[myvars]
+
+# correlation matrix and corrplot
+m=cor(data)
+corrplot(m,method="circle")
+
+# identified shot_distance and loc_y as having a correlation above .8
+cor(data$shot_distance,data$loc_y)
+
+# checked for autocorrelation by looking at the plot
+plot(cleaned$game_number,cleaned$shot_made_flag,xlab="Game Number",ylab="Shot Made")
+
+# linear regression 
+fit=lm(cleaned$shot_made_flag~cleaned$game_number+cleaned$loc_x+cleaned$minutes_remaining+cleaned$period+cleaned$playoffs+cleaned$seconds_remaining+cleaned$shot_distance+cleaned$win+cleaned$home+cleaned$three_pointer+cleaned$jump_shot+cleaned$dunk+cleaned$tip_shot+cleaned$hook_shot+cleaned$bank_shot)
+
+# backward stepwise 
+stepwise(fit,direction="backward",criterion="AIC")
+stepwise(fit,direction="backward",criterion="BIC")
+# model after removing least significant
+fit=lm(cleaned$shot_made_flag~cleaned$period+cleaned$seconds_remaining+cleaned$shot_distance+cleaned$win+cleaned$three_pointer+cleaned$jump_shot+cleaned$dunk+cleaned$tip_shot+cleaned$bank_shot)
+
+# checked diagnostics but you fail most of them becuase our Y variable is binary and we are trying to fit a linear model to it
+
+# logit
+fit=glm(cleaned$shot_made_flag~cleaned$game_number+cleaned$loc_x+cleaned$minutes_remaining+cleaned$period+cleaned$playoffs+cleaned$seconds_remaining+cleaned$shot_distance+cleaned$win+cleaned$home+cleaned$three_pointer+cleaned$jump_shot+cleaned$dunk+cleaned$tip_shot+cleaned$hook_shot+cleaned$bank_shot,family=binomial(logit))
+stepwise(fit,direction="backward",criterion="AIC")
+stepwise(fit,direction="backward",criterion="BIC")
+# removing least significant
+fit=glm(cleaned$shot_made_flag~cleaned$period+cleaned$seconds_remaining+cleaned$shot_distance+cleaned$win+cleaned$three_pointer+cleaned$jump_shot+cleaned$dunk+cleaned$tip_shot+cleaned$bank_shot,family=binomial(logit))
+
+
+# tested for stationarity
+adf.test(cleaned$avg, alternative = "stationary")
+
+# Probit Regressions on Wins 
+
+
