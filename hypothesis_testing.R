33f2eeb7b4430b54127b8f4fb476a7778fde71c0
jfelizardo <jfelizardo@college.harvard.edu>
2016-05-03 15:47:21 -0400

    Updated analysis of Kobe's performances in the clutch during wins versus losses, also in the clutch versus overall

--- hypothesis_testing.R
+++ hypothesis_testing.R
@@ -20,31 +20,21 @@ boot.mean = function(x,B) {
   return(list(boot.statistics = boot.statistics, interval=interval, se=se))
 }
 win_clutch_perc = c(rep(1,sum(clutch_shots_win$clutch_shots_made)), rep(0,sum(clutch_shots_win$clutch_shots_taken-clutch_shots_win$clutch_shots_made)))
-win_clutch_perc_ci = boot.mean(x = win_clutch_perc, B = 1000)
+win_clutch_perc_ci = boot.mean(x = win_clutch_perc, B = 1000)$interval
 loss_clutch_perc = c(rep(1,sum(clutch_shots_loss$clutch_shots_made)), rep(0,sum(clutch_shots_loss$clutch_shots_taken-clutch_shots_loss$clutch_shots_made)))
-loss_clutch_perc_ci = boot.mean(x = loss_clutch_perc, B = 1000)
+loss_clutch_perc_ci = boot.mean(x = loss_clutch_perc, B = 1000)$interval
 ## since the two confidence intervals do not overlap, there is significant evidence of a difference in Kobe's clutch field goal percentage in games won versus games lost. 
-
-boot_mean_difference = function(x,y){
-  A = sample(x, length(x), replace = T)
-  B = sample(y, length(y), replace = T)
-  return(mean(A)-mean(B))
-}
-clutch_win_mean_difference_data = replicate(1000, boot_mean_difference(x = win_clutch_perc, y = loss_clutch_perc))
-ad.test(clutch_win_mean_difference_data)
-## data looks to be Normally distributed
-## because confidence interval is analogous to t test output, calculate that instead
-clutch_mean_difference_ci = c(quantile(clutch_win_mean_difference_data, c(0.025,0.975))[1], quantile(clutch_win_mean_difference_data, c(0.025,0.975)[2]))
-## since confidence interval does not include 0, at a 0.05 significance level there is evidence of a difference in average shot percentage depending on whether the Lakers won or not.
+## based on the confidence intervals, Kobe's true clutch field goal percentage 
 ## so Kobe appears to have shot significantly better in the clutch during wins as opposed to losses.
 ad.test(bydate_data$avg)
+## to compare Kobe's field goal percentage in clutch situations versus his overall performance.
 ## in overall dataset, game-to-game shooting percentages are not Normally distributed, so bootstrap needs to be used.
-clutch_regular_mean_difference_data = replicate(1000, boot_mean_difference(x = clutch_shots$clutch_perc, y = bydate_data$avg))
-ad.test(clutch_regular_mean_difference_data)
-## data looks to be Normally distributed now.
-clutch_regular_mean_difference_ci = c(quantile(clutch_regular_mean_difference_data, c(0.025,0.975))[1], quantile(clutch_regular_mean_difference_data, c(0.025,0.975)[2]))
-## since confidence interval does not include 0, at a 0.05 significance level there is significant evidence of a difference in average shot percentage depending on whether Kobe is playing in a clutch situation.
-## since the confidence interval is negative, the data suggests that Kobe actually performs worse in clutch situations compared to his overall game performance.
+overall_shot_perc = c(rep(1, sum(bydate_data$shots_made)), rep(0, sum(bydate_data$shots_taken-bydate_data$shots_made)))
+overall_shot_perc_ci = boot.mean(x = overall_shot_perc, B = 1000)$interval
+clutch_shot_perc = c(rep(1,sum(clutch_shots$clutch_shots_made)), rep(0,sum(clutch_shots$clutch_shots_taken-clutch_shots$clutch_shots_made)))
+clutch_shot_perc_ci = boot.mean(x = clutch_shot_perc, B = 1000)$interval
+## since these confidence intervals do not overlap, at the 95% confidence level there is a significant difference between his field goal percentage in the clutch versus his overall performance.
+## based on these confidence intervals, the data suggests that Kobe actually performs worse during clutch situations compared to his overall performance.
 ## Finger injury occurred on February 5, 2008 season (season 11, overall game number 473, season ends at game 507)
 ## Compare Kobe's performance before the injury and after injury during the season. Hypothesis: Kobe is notoriously tough so he is not as affected by injuries.
 bydate_data_finger = subset(bydate_data, game_number > 472 & game_number < 508)
