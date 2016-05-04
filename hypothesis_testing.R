3776476104639b82eaf87d0950feb11bb4cf468b
jfelizardo <jfelizardo@college.harvard.edu>
2016-05-04 10:45:17 -0400

    Updated bootstrap analysis to ensure consideration of separate subsets of data

--- hypothesis_testing.R
+++ hypothesis_testing.R
@@ -29,23 +29,14 @@ loss_clutch_perc = c(rep(1,sum(clutch_shots_loss$clutch_shots_made)), rep(0,sum(
 loss_clutch_perc_ci = boot.mean(x = loss_clutch_perc, B = 1000)$interval
 ## since the two confidence intervals do not overlap, at the 95% confidence level there is significant evidence of a difference in Kobe's clutch field goal percentage in games won versus games lost. 
 ## so Kobe appears to have shot significantly better in the clutch during wins as opposed to losses.
-ad.test(bydate_data$avg)
-## to compare Kobe's field goal percentage in clutch situations versus his overall performance.
-## in overall dataset, game-to-game shooting percentages are not Normally distributed, so bootstrap needs to be used.
-overall_shot_perc = c(rep(1, sum(bydate_data$shots_made)), rep(0, sum(bydate_data$shots_taken)-sum(bydate_data$shots_made)))
-overall_shot_perc_ci = boot.mean(x = overall_shot_perc, B = 1000)$interval
-clutch_shot_perc = c(rep(1,sum(clutch_shots$clutch_shots_made)), rep(0,sum(clutch_shots$clutch_shots_taken)-sum(clutch_shots$clutch_shots_made)))
-clutch_shot_perc_ci = boot.mean(x = clutch_shot_perc, B = 1000)$interval
-## since these confidence intervals do not overlap, at the 95% confidence level there is a significant difference between his field goal percentage in the clutch versus his overall performance.
-## based on these confidence intervals, the data suggests that Kobe actually performs worse during clutch situations compared to his overall performance.
 ## Finger injury occurred on February 5, 2008 season (season 11, overall game number 473, season ends at game 507)
 ## Compare Kobe's performance before the injury and after injury during the season. Hypothesis: Kobe is notoriously tough so he is not as affected by injuries.
 bydate_data_finger = subset(bydate_data, game_number > 472 & game_number < 508)
-bydate_data_season11 = subset(bydate_data, game_number > 452 & game_number < 508)
+bydate_data_nofinger = subset(bydate_data, game_number > 452 & game_number < 473)
 finger_shot_perc = c(rep(1, sum(bydate_data_finger$shots_made)), rep(0, sum(bydate_data_finger$shots_taken)-sum(bydate_data_finger$shots_made)))
 finger_shot_perc_ci = boot.mean(x = finger_shot_perc, B = 1000)$interval
-season11_shot_perc = c(rep(1, sum(bydate_data_season11$shots_made)), rep(0, sum(bydate_data_season11$shots_taken)-sum(bydate_data_season11$shots_made)))
-season11_shot_perc_ci = boot.mean(x = season11_shot_perc, B = 1000)$interval
+nofinger_shot_perc = c(rep(1, sum(bydate_data_nofinger$shots_made)), rep(0, sum(bydate_data_nofinger$shots_taken)-sum(bydate_data_nofinger$shots_made)))
+nofinger_shot_perc_ci = boot.mean(x = nofinger_shot_perc, B = 1000)$interval
 ## since the confidence intervals overlap, the data does not provide enough evidence to show that Kobe performed significantly differently before and after he suffered the finger injury.
 ## note that although Kobe Bryant played all 82 games, dataset only includes 55 total games from the 2007-2008 season.
 ## also note that the confidence interval of Kobe's performance when he played through the injury is wider, likely due to the smaller sample size of only 35 games provided in the dataset.
